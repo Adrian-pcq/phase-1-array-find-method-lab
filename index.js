477b40800a1c780e62cbf672f077423b347b967a
Adrian <adrianqueizan@gmail.com>
2023-08-23 16:44:00 -0400

    done array find lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array){
+ const w = array.find(array => array.result==="W")
+   if(w){
+    return w.year
+   }else {return undefined}
+
+ 
+
+} 
+
